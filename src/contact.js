71a88c86db5e8cfd19b6655e9cfe598590b7faf6
bokamix <janiakpol@gmail.com>
2019-05-03 17:01:10 +0200

    add messenger chat on website

--- src/contact.js
+++ src/contact.js
@@ -10,9 +10,9 @@ class ContactForm extends Component {
 
        <div className="containercenter m-5"> 
         
-        <h3 className="h2section">Skontaktuj się ze mna za pomocą mojego bota w messengerze Kotlecik
+        <h3 className="h2section">Skontaktuj się ze mna za pomocą mojego bota w messengerze.
     </h3>
-        <h4>lub przez mój profil LinkedIn.</h4>
+        <h4>lub przez mój profil <a href="https://www.linkedin.com/in/wojciech-janiak-7a7808136/">LinkedIn.</a></h4>
 
         
         </div>
