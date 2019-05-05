d75b03a0459b24f28e117a7ed2c790cd35e057a5
bokamix <janiakpol@gmail.com>
2019-05-05 15:06:03 +0200

    Uzupełnienie treści

--- src/contact.js
+++ src/contact.js
@@ -10,10 +10,10 @@ class ContactForm extends Component {
 
        <div className="containercenter m-5"> 
         
-        <h3 className="h2section">Skontaktuj się ze mna za pomocą mojego bota w messengerze.
+        <h3 className="h2section">Skontaktuj się ze mna za pomocą messengerza.
     </h3>
         <h4>lub przez mój profil <a href="https://www.linkedin.com/in/wojciech-janiak-7a7808136/">LinkedIn.</a></h4>
-
+        <h4>Adres email: janiakpol@gmail.com</h4>
         
         </div>
     );
