170c09ae5ddf2c6eed17a665861c77288033b829
bokamix <janiakpol@gmail.com>
2019-05-05 15:23:40 +0200

    poprawienie błędów

--- src/containersecond.js
+++ src/containersecond.js
@@ -8,7 +8,7 @@ class Containersecond extends Component {
 
       <div id="containersecond" className="secondsectiondiv" >   
                     <h2 className="h2section">Czy twoja obecność <br /> w sieci zarabia dla Ciebie?</h2>
-                    <p className="psection">Jeśli szukasz pomocy, w zwiększeniu potencjału twojej strony to w tym mogę Ci pomóc.</p>
+                    <p className="psection">Jeśli szukasz pomocy w zwiększeniu potencjału swojej strony internetowej, mogę Ci w tym pomóc.</p>
                       <div className="bnt-jede-main"><a href="#containerportfolioid" className="btn btn-outline btn-xl js-scroll-trigger">Zobacz portfolio</a></div>
            
         </div>
